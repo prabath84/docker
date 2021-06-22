5a0e410f5cfdc999f0280dd7e8b3a3f570d2608e
Prabath Anuradha <anuradha.prabath@gmail.com>
2021-06-22 12:39:44 +0530

    f comm

---
+++ dockertut/Dockerfile
@@ -0,0 +1,8 @@
+FROM node:alpine
+
+COPY . /app
+
+WORKDIR /app
+
+CMD node app.js
+
---
+++ node-example3/app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hi Prabath!!!'))
+app.listen(3000, () => console.log('Server ready'))
